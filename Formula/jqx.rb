b414e63e19b868a83c67330c4f8fb4f330f9cb4f
Bruno Bonacci <bruno.bonacci@gmail.com>
2019-05-04 15:35:47 +0100

    update jqx sha

--- Formula/jqx.rb
+++ Formula/jqx.rb
@@ -2,7 +2,7 @@ class Jqx < Formula
   desc     "A command line tool to process json, like jq but with ClojureScript expressions."
   homepage "https://github.com/BrunoBonacci/homebrew-lazy-tools.git/tree/master/scripts/jqx"
   url      "https://github.com/BrunoBonacci/homebrew-lazy-tools/releases/download/jqx-0.1.0/jqx-homebrew.tar.gz"
-  sha256   "6552af356fb48260b9e5c6d97d1ed7477391aacd1e5f11b22be5bd96eeaeb3e6"
+  sha256   "bdcbdfa7a9ca052783babc57a1671f828a9984e38509513aa2f8f2aa9327b057"
   version  "0.1.0"
 
   depends_on "planck"
