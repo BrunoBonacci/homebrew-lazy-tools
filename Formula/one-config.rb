777470243dcf5f69b2bc1ae98f49f3be46cd9b0a
Bruno Bonacci <bruno.bonacci@gmail.com>
2019-05-05 18:22:15 +0100

    fix url

--- Formula/one-config.rb
+++ Formula/one-config.rb
@@ -4,8 +4,8 @@
 class OneConfig < Formula
   desc     "A command-line tool to manage multiple environments and application configuration safely and effectively."
   homepage "https://github.com/BrunoBonacci/1config.git"
-  url      "https://github.com/BrunoBonacci/1config/releases/download/0.9.2/1cfg"
-  #sha256   "ca0673d7435148fe0073af084d6907d11f8b6488ebbf44a9725e99c5436338ea"
+  url      "https://github.com/BrunoBonacci/1config/releases/download/0.9.2/1cfg-homebrew.tar.gz"
+  sha256   "ca0673d7435148fe0073af084d6907d11f8b6488ebbf44a9725e99c5436338ea"
   version  "0.9.2"
 
   bottle :unneeded
