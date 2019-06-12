35f560a3fadf7cbb4df2a990a252f71a8d93fe6a
Bruno Bonacci <bruno.bonacci@gmail.com>
2019-06-12 23:44:15 +0100

    update synapse to v0.5.0

--- Formula/synapse.rb
+++ Formula/synapse.rb
@@ -5,12 +5,10 @@ class Synapse < Formula
 
   desc          "Synapse is the smart way to connect and configure docker containers"
   homepage      "https://github.com/BrunoBonacci/synapse.git"
-  url           "https://github.com/BrunoBonacci/synapse/releases/download/0.4.0/synapse-homebrew-#{OS}-x86_64.tar.gz"
-  version       "0.4.0"
-  sha256_Darwin "6552af356fb48260b9e5c6d97d1ed7477391aacd1e5f11b22be5bd96eeaeb3e6"
-  sha256_Linux  "371ae031e5e7ba378b0e597d43ed27a6e17f45b5694a58ab04ed489ddc279706"
-
-  depends_on "curl"
+  url           "https://github.com/BrunoBonacci/synapse/releases/download/0.5.0/synapse-homebrew-#{OS}-x86_64.tar.gz"
+  version       "0.5.0"
+  sha256_Darwin "c436ec4f561313bb78ee68e302f3ca91920f3becf135ea6964335bef15b8f921"
+  sha256_Linux  "be680cd3f0b194c858b045987c8fda2e87f38d30e5d1de486703c11d9e11f744"
 
   bottle :unneeded
 
