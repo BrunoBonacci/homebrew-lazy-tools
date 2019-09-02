77a135b4111740d36c08231fc93507ff8abc5b28
Bruno Bonacci <bruno.bonacci@gmail.com>
2019-09-02 23:21:24 +0100

    update rolling-update to v0.2.2

--- Formula/rolling-update.rb
+++ Formula/rolling-update.rb
@@ -4,9 +4,9 @@
 class RollingUpdate < Formula
   desc     "A command line tool for automated rolling update of auto-scaling groups."
   homepage "https://github.com/BrunoBonacci/rolling-update"
-  url      "https://github.com/BrunoBonacci/rolling-update/releases/download/0.2.1/rolling-update-homebrew.tar.gz"
-  sha256   "07ea390c2405ce9e8b8885c7ea58b6c39416b82a8e973bd6fa745938f2fc45cb"
-  version  "0.2.1"
+  url      "https://github.com/BrunoBonacci/rolling-update/releases/download/0.2.2/rolling-update-homebrew.tar.gz"
+  sha256   "a1a36b19583da0554510a98c9d3f37190e2bcab5a32d25ce70b606bfa6e89c63"
+  version  "0.2.2"
 
   bottle :unneeded
 
