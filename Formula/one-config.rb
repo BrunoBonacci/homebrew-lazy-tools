a37cf79d73fb837da7676d07e80f366184e39722
Bruno Bonacci <bruno.bonacci@gmail.com>
2020-02-09 20:10:11 +0000

    update 1config to 0.15.0

--- Formula/one-config.rb
+++ Formula/one-config.rb
@@ -4,14 +4,16 @@
 class OneConfig < Formula
   desc     "A command-line tool to manage multiple environments and application configuration safely and effectively."
   homepage "https://github.com/BrunoBonacci/1config.git"
-  url      "https://github.com/BrunoBonacci/1config/releases/download/0.10.3/1cfg-homebrew.tar.gz"
-  sha256   "4c1f5c4c35739fe3ac191dc6b889f670c1dd0f438c1bd555f6c590961966369e"
-  version  "0.10.3"
+  url      "https://github.com/BrunoBonacci/1config/releases/download/0.15.0/1cfg-homebrew.tar.gz"
+  sha256   "95bf87c84146ea99013ca46f5decae373541b6948e9d22d8aec65131e7b25a65"
+  version  "0.15.0"
 
   bottle :unneeded
 
   def install
     bin.install "1cfg"
     bin.install "1cfg.jar"
+    bin.install "1cfg-ui-beta"
+    bin.install "1cfg-ui-beta.jar"
   end
 end
