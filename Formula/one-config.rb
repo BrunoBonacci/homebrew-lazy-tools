bbe829b1434185f7ab5f84154b2273f2ae79feec
Bruno Bonacci <bruno.bonacci@gmail.com>
2020-03-18 17:11:14 +0000

    update oneconfig to 0.16.1

--- Formula/one-config.rb
+++ Formula/one-config.rb
@@ -4,9 +4,9 @@
 class OneConfig < Formula
   desc     "A command-line tool to manage multiple environments and application configuration safely and effectively."
   homepage "https://github.com/BrunoBonacci/1config.git"
-  url      "https://github.com/BrunoBonacci/1config/releases/download/0.16.0/1cfg-homebrew.tar.gz"
-  sha256   "38696a3951dc6f71fdc712f2a8ac8fbd9911ad5dc4e1ad193566a9d5bab960c9"
-  version  "0.16.0"
+  url      "https://github.com/BrunoBonacci/1config/releases/download/0.16.1/1cfg-homebrew.tar.gz"
+  sha256   "157d1dbe47020c5516f1304644546db72df44956413a626504d13c96a5a116d4"
+  version  "0.16.1"
 
   bottle :unneeded
 
